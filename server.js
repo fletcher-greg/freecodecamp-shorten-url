b1d72a0e985a55c1fd0c6dbfee6fd1ab896c0f79
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-11-08 15:44:47 +0100

    aded url redirecting

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const util = require('util');
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +8,7 @@ app.use(express.urlencoded());
 
 app.use(express.static('public'));
 
+let redirectUrl;
 const urlRegex = url => {
   const regexFormat = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
   const regex = new RegExp(regexFormat);
@@ -37,14 +37,22 @@ const returnAnswer = async (req, res) => {
   if (!urlRegex(url)) return res.json(invalidURL);
 
   try {
-    let resolving = await checkDNS(url);
-    return res.json({ original_url: url, shortened_url: url.length });
+    await checkDNS(url);
+
+    redirectUrl = { original_url: url, shortened_url: url.length };
+    return res.json(redirectUrl);
   } catch {
     res.json(invalidURL);
   }
 };
 
 app.get('/api', (req, res) => res.json({ hi: 'hi' }));
+
+app.get('/api/shorturl/', (req, res) =>
+  redirectUrl
+    ? res.status(301).redirect(redirectUrl.original_url)
+    : res.send('Please enter a valid url first')
+);
 
 app.post('/api/url-shortner', returnAnswer);
 
